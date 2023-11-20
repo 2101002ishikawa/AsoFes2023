2f4dbf93925890e51faf3e3c1866e5092301c3b2
OkazakiKeito <2301144@s.asojuku.ac.jp>
2023-11-20 17:14:51 +0900

    php
    phpの作業をしました

--- check.php
+++ check.php
@@ -4,23 +4,26 @@
                 'mysql:host=localhost;
                 dbname=AsoFes;
                 charset=utf8',   
-                'データベースでのユーザID',
-                'データベースでのパスワード'
-            );
+                'asofes',
+                'asofes2023'
+    );
 
     //sqlの記述
-    $sql = "SELECT * FROM aso WHERE AsoName = :AsoName";
-
+    $sql = "INSERT INTO `students`(`student_id`, `student_name`, `daytime`, `flag`) VALUES (:student_id,:student_name,:daytime,'Y')";
+ 
     // sqlにデータを埋め込めるようにする
     $ninni = $dao->prepare($sql);
 
     //sqlに必要なデータを埋め込む
-    $ninni->bindValue(":AsoName","入れたいデータ",PDO::PARAM_STR);
+    $ninni->bindValue(":student_id",$_POST['n1'],PDO::PARAM_STR);
+    $ninni->bindValue(":student_name",$_POST['n2'],PDO::PARAM_STR);
+    $ninni->bindValue(":daytime",date("Y-m-d H:i:s"),PDO::PARAM_STR);
 
     //実行する
     $ninni->execute();
 
     //実行結果によってページを遷移させる
-    $data = $ninni->fetchAll();
+   
+   header('Location:'.'botan1.html');
 
 ?>
